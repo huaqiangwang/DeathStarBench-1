6ff66c8ff4db5e1ebab95cbc97148d3b968623ee
Lianhao Lu <lianhao.lu@intel.com>
2021-02-06 17:23:14 +0800

    hotelReservation: launch_wrk enhancement
    - support more parameters
    - support binding wrk to specific cpu cores
    
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

--- hotelReservation/lauch_wrk.sh
+++ hotelReservation/lauch_wrk.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
-[[ "$#" -ne 2 ]] && echo "Usage: $0 <duration> <rate_per_sec> " && exit 1
-
 DIR=$(dirname -- "$0")
-DURATION="$1"
-QPS="$2"
+EXECNAME=$(basename -- "$0")
+
+THREADS=10
+CONNS=50
+
+CORENUM="$(nproc)"
+
+CPUS="0-${CORENUM}"
+DURATION=""
+QPS=""
+
 
 OUTPUTDIR=`mktemp -d`
-
-TOTAL=`docker-compose -f ${DIR}/docker-compose.yml ps | grep frontend | wc -l`
 
 declare -a PIDS
 declare -a OUTPUTS
 
+
+function usage() {
+    echo "$EXECNAME: [ -t <threads> ] [ -c <connections> ] [ -C '<cpulist>' ]  -d <duration>  -R <QPS>"
+    echo "    <threads>       number of threads, default is 10"
+    echo "    <connections>   number of connections, default is 50"
+    echo "    <cpulist>       cpu list to bind wrk to, default is 0-${CORENUM}"
+    echo "    <duration>      how many seconds to run"
+    echo "    <QPS>           request per second"
+    exit $1
+}
 
 function get_port {
   let "p = $1 + 5000"
@@ -21,7 +36,7 @@ function get_port {
 
 function launch_wrk {
   port=$(get_port $1)
-  ${DIR}/../mediaMicroservices/wrk2/wrk -D exp -t 16 -c 16 -d $DURATION -L -s ${DIR}/wrk2_lua_scripts/mixed-workload_type_1.lua -R $QPS http://localhost:${port} > ${OUTPUTDIR}/frontend$1 &
+  taskset -c "${CPUS}" ${DIR}/../mediaMicroservices/wrk2/wrk -D exp -t ${THREADS}  -c ${CONNS} -d ${DURATION} -L -s ${DIR}/wrk2_lua_scripts/mixed-workload_type_1.lua -R $QPS http://localhost:${port} > ${OUTPUTDIR}/frontend$1 &
   pid=$!
   PIDS+=("$pid")
   OUTPUTS+=("${OUTPUTDIR}/frontend$1")
@@ -44,6 +59,27 @@ function print_all {
 	done
 }
 
+
+function parse_args {
+    while getopts "t:c:R:d:C:h" opt; do
+        case $opt in
+	  t) THREADS=$OPTARG;;
+	  c) CONNS=$OPTARG;;
+	  R) QPS=$OPTARG;;
+	  d) DURATION=$OPTARG;;
+	  C) CPUS="$OPTARG";;
+	  h) usage 0;;
+        esac
+    done
+
+    [[ "x${QPS}" == "x" ]] && usage 1
+    [[ "x${DURATION}" == "x" ]] && usage 1
+}
+
+parse_args $@
+
+TOTAL=`docker-compose -f ${DIR}/docker-compose.yml ps | grep frontend | wc -l`
+echo "Totally frontend count: ${TOTAL}"
 
 for (( c=0; c<${TOTAL}; c++ ))
 do
