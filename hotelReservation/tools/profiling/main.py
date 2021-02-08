63341eda74fdda91a5386678045a9debede999ef
huaqiang.wang <huaqiang.wang@intel.com>
2021-02-08 18:03:31 +0800

    tool: Add command line parser to support more profiling tools.
    Signed-off-by: huaqiang.wang <huaqiang.wang@intel.com>

--- hotelReservation/tools/profiling/main.py
+++ hotelReservation/tools/profiling/main.py
@@ -1,7 +1,36 @@
 import logger
 import perf
+import argparse
+
+
+def process_args():
+    parser = argparse.ArgumentParser(description='DSB performance profoling tools.')
+    parser.add_argument('command',
+     help='''Specify the profiling command, valid command is "flamegraph".\n\n'
+    'flamegraph: \tGenerate flame graphs for all uService processes as well as the '
+    'system wide function call stack.'''
+    )
+
+    args = parser.parse_args()
+
+    if args.command in ['flamegraph']:
+        return args.command
+    else:
+        parser.print_help()
+        return
+
+def command_handler(cmd):
+    if cmd is None:
+        return
+
+    if cmd == 'flamegraph':
+        perf.create_all_flame_graphs()
 
 
 if __name__ == "__main__":
     logger.init()
-    perf.create_all_flame_graphs()
+       
+    cmd = process_args()
+    command_handler(cmd)
+
+    
