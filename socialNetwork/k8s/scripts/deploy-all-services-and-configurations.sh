9caca632ab2d00e181f2aa325d74ae420390e06c
Wang Huaqiang <huaqiang.wang@intel.com>
2021-03-03 17:31:56 +0800

    k8s: Not neccessary to expose the serices
    jarger-out and nginx-thrift services are not necessary to
    be exposed.
    
    Signed-off-by: Wang Huaqiang <huaqiang.wang@intel.com>

--- socialNetwork/k8s/scripts/deploy-all-services-and-configurations.sh
+++ socialNetwork/k8s/scripts/deploy-all-services-and-configurations.sh
@@ -14,8 +14,8 @@ for service in *.yaml ;  do
   kubectl apply -f $service -n ${NS}
 done
 
-kubectl expose service nginx-thrift -n ${NS}
-kubectl expose service jaeger-out -n ${NS}
+# kubectl expose service nginx-thrift -n ${NS}
+# kubectl expose service jaeger-out -n ${NS}
 
 echo "After all pods are running (oc get pods):"
 echo "Follow the instructions in openshift/README.md to configure and run init_social_graph.py to load the dataset."
