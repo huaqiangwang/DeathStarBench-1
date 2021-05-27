bc9466a92def55734dfa6863e040bc79eed4182e
Xie Zhihao <zhihao.xie@intel.com>
2021-05-27 17:08:38 +0800

    Add option enabling TLS for K8s

--- socialNetwork/k8s/scripts/deploy-all-services-and-configurations.sh
+++ socialNetwork/k8s/scripts/deploy-all-services-and-configurations.sh
@@ -5,7 +5,8 @@ NS="social-network"
 
 kubectl create namespace ${NS}
 
-./scripts/create-all-configmap.sh 
+python3 ./scripts/config.py
+./scripts/create-all-configmap.sh
 
 # The following script optionally creates local docker images suitable for local customization.
 # ./scripts/build-docker-img.sh
