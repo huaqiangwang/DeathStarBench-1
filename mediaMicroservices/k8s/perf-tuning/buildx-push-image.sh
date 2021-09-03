2e2db656a97de1e9ed6425f1684cd396f11342cf
huaqiang.wang <huaqiang.wang@intel.com>
2021-09-03 14:06:07 +0000

    disable loopback by defualt

--- mediaMicroservices/k8s/perf-tuning/buildx-push-image.sh
+++ mediaMicroservices/k8s/perf-tuning/buildx-push-image.sh
@@ -1,13 +1,13 @@
 #!env bash
 
 # build images
-PLATS="linux/amd64"
+#PLATS="linux/amd64"
 #PLATS="linux/arm64"
-#PLATS="linux/arm64,linux/amd64"
+PLATS="linux/arm64,linux/amd64"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name builder --driver docker-container --use
 docker buildx inspect --bootstrap
 #docker buildx build --platform $PLATS -t aenniwang/thrift-microservice-media-deps:xenial -f ../../docker/thrift-microservice-deps/cpp/Dockerfile ../../docker/thrift-microservice-deps --push
 #docker buildx build  --platform $PLATS -t aenniwang/openresty-thrift-media:xenial -f ../../docker/openresty-thrift/xenial/Dockerfile ../../docker/openresty-thrift --push
-docker buildx build  --platform $PLATS -t aenniwang/mediamicroservices:loop2 -f ../../Dockerfile ../../ --push
+docker buildx build  --platform $PLATS -t aenniwang/mediamicroservices:test -f ../../Dockerfile ../../ --push
 
